c5945d8febee37146496350ae41a6858287ffd2a
Valmir Dornas <fernandocursos2021@gmail.com>
2025-09-29 22:06:00 -0300

    Início do projeto

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'vue3-toastify/dist/index.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import App from './App.vue';
+import router from './router';
 
 const app = createApp(App)
 
