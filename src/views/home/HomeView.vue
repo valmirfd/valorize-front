df238b3abd6f849eb1d69f934eb8f162565b1e4b
Valmir Dornas <fernandocursos2021@gmail.com>
2025-09-30 06:53:59 -0300

    Innício da NavBar

---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+    <div class="container-fluid mt-2">
+        <h3 class="mb-2 text-muted">Você está na Home</h3>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
